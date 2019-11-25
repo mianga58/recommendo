a527c363626364eed01ef8ef536585ee535450d8
mainga <49961392+mianga58@users.noreply.github.com>
2019-11-25 17:59:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -135,9 +135,9 @@ def login():
                error = 'Username not found'
                return render_template("login.html", error=error)
 
-          return render_template('login.html')
+      return render_template('login.html')
            # close connection
-          cur.close()
+      cur.close()
 
 #check if user logged in
 def is_logged_in(f):

786ca2d39fd79fb6db1b282814571eded7c49fb5
mainga <49961392+mianga58@users.noreply.github.com>
2019-11-25 23:54:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
 from flaskext.mysql import MySQL
+import numpy as np
+import pandas as pd
+
+# libraries for making count matrix and similarity matrix
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 app = Flask(__name__)
@@ -20,6 +26,51 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL()
 mysql.init_app(app)
 
+# define a function that creates similarity matrix
+# if it doesn't exist
+def create_sim():
+    data = pd.read_csv('data.csv')
+    # creating a count matrix
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(data['comb'])
+    # creating a similarity score matrix
+    sim = cosine_similarity(count_matrix)
+    return data,sim
+
+# defining a function that recommends 10 most similar movies
+def rcmd(m):
+    m = m.lower()
+    # check if data and sim are already assigned
+    try:
+        data.head()
+        sim.shape
+    except:
+        data, sim = create_sim()
+    # check if the movie is in our database or not
+    if m not in data['movie_title'].unique():
+        return('This movie is not in our database.\nPlease check if you spelled it correct.')
+    else:
+        # getting the index of the movie in the dataframe
+        i = data.loc[data['movie_title']==m].index[0]
+
+        # fetching the row containing similarity scores of the movie
+        # from similarity matrix and enumerate it
+        lst = list(enumerate(sim[i]))
+
+        # sorting this list in decreasing order based on the similarity score
+        lst = sorted(lst, key = lambda x:x[1] ,reverse=True)
+
+        # taking top 1- movie scores
+        # not taking the first index since it is the same movie
+        lst = lst[1:11]
+
+        # making an empty list that will containg all 10 movie recommendations
+        l = []
+        for i in range(len(lst)):
+            a = lst[i][0]
+            l.append(data['movie_title'][a])
+        return l
+
 @app.route('/')
 def index():
     return render_template("home.html")
@@ -30,7 +81,15 @@ def profile():
 
 @app.route("/predictor", methods=['GET','POST'])
 def predictor():
-  
+    movie = request.args.get('movie')
+    r = rcmd(movie)
+    movie = movie.upper()
+    if type(r)==type('string'):
+        return render_template('predictor.html',movie=movie,r=r,t='s')
+    else:
+        return render_template('predictor.html',movie=movie,r=r,t='l')
+
+
     return render_template("predictor.html")
 
 @app.route("/about")

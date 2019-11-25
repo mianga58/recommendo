00322cfb82b05b0cafe35a51daf38901e5d6d663
mainga <49961392+mianga58@users.noreply.github.com>
2019-11-25 19:59:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ def login():
             #Create cursor
             cur = mysql.get_db().cursor()
 
-          #Get user by username
+            #Get user by username
             result = cur.execute("SELECT * FROM users WHERE username = %s",
                                  [username])
 
@@ -135,9 +135,9 @@ def login():
                error = 'Username not found'
                return render_template("login.html", error=error)
 
-return render_template('login.html')
+           return render_template('login.html')
            # close connection
-cur.close()
+           cur.close()
 
 #check if user logged in
 def is_logged_in(f):
